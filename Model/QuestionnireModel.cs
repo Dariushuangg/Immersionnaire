54acb621450d9d5ffe8ad4fb77e6cf9b2480b1b3
Darius_sh711 <sh711@duke.edu>
2023-01-09 08:48:34 -0800

    fixed glittering issue by modifying size of collider

--- Model/QuestionnireModel.cs
+++ Model/QuestionnireModel.cs
@@ -11,6 +11,7 @@ public class QuestionnireModel : MonoBehaviour
     /* Get the data of a specific question for the caller to display.*/
     public Question GetQuestionAt(int index) {
         if (index > questions.Count - 1) throw new Exception("Index out of range for questions: index = " + index);
+        Util.SetDebugLog("GetQuestionAt index", index.ToString(), true);
         return questions[index];
     }
 
