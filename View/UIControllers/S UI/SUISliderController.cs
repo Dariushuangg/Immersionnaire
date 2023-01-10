1a22cd16c18293efbe0f52628194836b46f8118a
Darius_sh711 <sh711@duke.edu>
2023-01-10 16:44:27 -0700

    #3; Implemented slider controller APIs

---
+++ View/UIControllers/S UI/SUISliderController.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class SUISliderController : MonoBehaviour
+{
+    private UnityEvent<int, float> SliderValueChanged;
+    private GameObject SliderTorus;
+    private GameObject SliderValueDisplayer;
+    private Transform ActiveGrabberTransform;
+    private bool IsActive;
+    private int SliderID;
+
+    void Update()
+    {
+        if (IsActive) FollowActiveGrabber();
+    }
+
+    public void InitController() 
+    {
+        SliderValueChanged = new UnityEvent<int, float>();
+        SliderTorus = gameObject.transform.Find("SliderTorus").gameObject;
+        SliderValueDisplayer = gameObject.transform.Find("SliderValueDisplayer").gameObject;
+        IsActive = false;
+        SliderID = int.Parse(gameObject.name.Last().ToString());
+        Util.SetDebugLog("CONVERT check: ", SliderID + "", true);
+    }
+
+    /// <summary>
+    /// Cache the active grabber's transform for the slider bar to follow.
+    /// </summary>
+    /// <param name="ActiveGrabberTransform">Transform of the active grabber of the collider.</param>
+    public void SetGrabberTo(Transform ActiveGrabberTransform)
+    {
+        this.ActiveGrabberTransform = ActiveGrabberTransform;
+    }
+
+    private void FollowActiveGrabber() { }
+}
