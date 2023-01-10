4245544700487c6cde4e0ae3f6c7a6ca278a5751
Darius_sh711 <sh711@duke.edu>
2023-01-10 16:13:21 -0700

    initial commit for #3

---
+++ View/UIControllers/S UI/SUISliderBarController.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SUISliderBarController : MonoBehaviour
+{
+    private GameObject SliderTorus;
+    private GameObject SliderValueDisplayer;
+    private Transform ActiveGrabberTransform;
+    private bool IsActive;
+}
