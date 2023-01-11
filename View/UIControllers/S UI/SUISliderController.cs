997d636e7b0413b47082817c9e361ba519d7c50d
Darius_sh711 <sh711@duke.edu>
2023-01-10 17:57:16 -0700

    #3; Completed location calculation function; Completed runnable inetgration

--- View/UIControllers/S UI/SUISliderController.cs
+++ View/UIControllers/S UI/SUISliderController.cs
@@ -9,6 +9,7 @@ public class SUISliderController : MonoBehaviour
     private UnityEvent<int, float> SliderValueChanged;
     private GameObject SliderTorus;
     private GameObject SliderValueDisplayer;
+    private GameObject SliderCollider;
     private Transform ActiveGrabberTransform;
     private bool IsActive;
     private int SliderID;
@@ -23,9 +24,10 @@ public class SUISliderController : MonoBehaviour
         SliderValueChanged = new UnityEvent<int, float>();
         SliderTorus = gameObject.transform.Find("SliderTorus").gameObject;
         SliderValueDisplayer = gameObject.transform.Find("SliderValueDisplayer").gameObject;
+        SliderCollider = gameObject.transform.Find("SliderCollider").gameObject;
         IsActive = false;
         SliderID = int.Parse(gameObject.name.Last().ToString());
-        Util.SetDebugLog("CONVERT check: ", SliderID + "", true);
+        Util.SetDebugLog("slider id check: ", SliderID + "", true);
     }
 
     /// <summary>
@@ -42,10 +44,25 @@ public class SUISliderController : MonoBehaviour
     {
         ActiveGrabberTransform = null;
         IsActive = false;
+
+        // Reset collider's position
+        SliderCollider.transform.position = SliderTorus.transform.position;
+        SliderCollider.transform.rotation = Quaternion.identity; // TODO: Rotate by 90 degrees in X
     }
 
     /// <summary>
     /// Follow active grabber and broadcast slider value change event. 
     /// </summary>
-    private void UpdateSliderStatus() { }
+    private void UpdateSliderStatus() 
+    {
+        Vector3 localChanges = gameObject.transform.InverseTransformPoint(ActiveGrabberTransform.position);
+        // 0.62 -> -0.81 // Magic Number. Bad! To-be refactor into property files in future version
+        float newX = Mathf.Clamp(localChanges.x, -0.81f, 0.62f);
+        SliderTorus.transform.localPosition = new Vector3(newX,
+            SliderTorus.transform.localPosition.y,
+            SliderTorus.transform.localPosition.z);
+        SliderValueDisplayer.transform.localPosition = new Vector3(newX,
+            SliderValueDisplayer.transform.localPosition.y,
+            SliderValueDisplayer.transform.localPosition.z);
+    }
 }
